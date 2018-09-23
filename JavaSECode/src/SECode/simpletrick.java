5d014712e24270a2535e078d163d9be428f74d64
naveenroshan <naveenroshan93@gmail.com>
2018-09-23 21:27:31 +0530

    simple

--- JavaSECode/src/SECode/simpletrick.java
+++ JavaSECode/src/SECode/simpletrick.java
@@ -13,12 +13,20 @@ public class simpletrick {
 	// the methods setName
 
 	public static void main(String[] args) {
-		// float avg = (2+3)/2;
-		// long avg = (2 + 3)/2;
+		// long x ,y;
+		// float x,y;
 		double x, y;
 		x = 2.0;
 		y = 3.0;
+		// type one
 		double avg = (x + y) / 2;
+
+		// type two
+		// float avg = (x + y) / 2;
+
+		// type three
+		// long avg = (x + y) / 2;
+
 		System.out.println(avg);
 		simpletrick t = new simpletrick();
 		setName();
