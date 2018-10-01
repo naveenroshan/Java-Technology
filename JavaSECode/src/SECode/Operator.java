3ee7c2008028195b15bf4917f605c905681ab72b
unknown <mikejackson9321@gmail.com>
2018-10-01 16:54:01 +0530

    newcodeedited

--- JavaSECode/src/SECode/Operator.java
+++ JavaSECode/src/SECode/Operator.java
@@ -1,12 +1,15 @@
 package SECode;
+
 /**
  *
  * @author Naveen
  */
-
-public class BitWiseOperator {
+// here we see the example of the bit wise ,ternary operator 
+public class Operator {
 
     public static void main(String[] args) {
+
+	// bit wise operator example:
 	String binary[] = { "0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", "1000", "1001", "1010",
 		"1011", "1100", "1101", "1110", "1111" };
 	int a = 3; // 0 + 2 + 1 or 0011 in binary
@@ -24,13 +27,23 @@ public class BitWiseOperator {
 	System.out.println("~a&b|a&~b = " + binary[f]);
 	System.out.println(" ~a = " + binary[g]);
 	
-	
-	//ternary operator 
-	int q=1;
-	int w =2;
-	int r =3;
-	if(q==1? w!=1:r==1) {
+
+	// ternary operator type one
+	int q = 1;
+	int w = 2;
+	int r = 3;
+	if (q == 1 ? w != 1 : r == 1) {
 	    System.out.println("worked");
+
+	    // ternary operator type two
+	    int i, k;
+	    i = 10;
+	    k = (i < 0) ? i : -i; // get absolute value of i
+	    System.out.println("Absolute value of " + i + " is " + k);
+	    
+	    i = -10;
+	    k = (i < 0) ? -i : i; // get absolute value of i
+	    System.out.print("Absolute value of " + i + " is " + k);
 	}
     }
 }
