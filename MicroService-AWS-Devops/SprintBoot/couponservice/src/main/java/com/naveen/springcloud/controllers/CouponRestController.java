6d44360a1c73fbd9ea41c1bbf4463b1d133fff88
Hmnaveen <naveen.s2@happiestminds.com>
2021-11-26 10:19:42 +0530

    spring boot Aws devops

---
+++ MicroService-AWS-Devops/SprintBoot/couponservice/src/main/java/com/naveen/springcloud/controllers/CouponRestController.java
@@ -0,0 +1,34 @@
+package com.naveen.springcloud.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.naveen.springcloud.model.Coupon;
+import com.naveen.springcloud.repos.CouponRepo;
+
+@RestController
+@RequestMapping("/couponApi")
+public class CouponRestController {
+	
+	// Injecting the interface coupon repo for implementation
+	@Autowired
+	CouponRepo repo;
+	
+	// creating a coupon given from client and storing in the DB
+	@RequestMapping(value = "/coupons", method = RequestMethod.POST)
+	public Coupon create(@RequestBody Coupon coupon) {
+		return repo.save(coupon);
+	}
+
+	// get the coupon from the data base which the client requested code
+	@RequestMapping(value = "/coupons/{code}", method = RequestMethod.GET)
+	public Coupon getCoupon(@PathVariable("code") String code) {
+		return repo.findByCode(code);
+		
+	}
+	
+}
---
+++ MicroService-AWS-Devops/SprintBoot/couponservice/src/main/java/com/naveen/springcloud/repos/CouponRepo.java
@@ -0,0 +1,13 @@
+package com.naveen.springcloud.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.naveen.springcloud.model.Coupon;
+
+// this interface is used to perform the quered operation in the mysql table coupon.
+public interface CouponRepo extends JpaRepository<Coupon, Long> {
+
+	//this spring method from user define method performance the select coupon base on the code.
+	Coupon findByCode(String code);
+
+}
