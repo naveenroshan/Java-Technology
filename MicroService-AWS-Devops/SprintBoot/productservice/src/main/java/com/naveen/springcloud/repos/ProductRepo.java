96dc970016863eb16bc264ee7fbf75f3a0e583dd
NaveenRoshan <thanks@NaveenRoshans-MacBook-Pro.local>
2022-01-12 14:14:39 +0530

    update

--- MicroService-AWS-Devops/SprintBoot/productservice/src/main/java/com/naveen/springcloud/repos/ProductRepo.java
+++
@@ -1,9 +0,0 @@
-package com.naveen.springcloud.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.naveen.springcloud.model.Product;
-
-public interface ProductRepo extends JpaRepository<Product, Long> {
-
-}
