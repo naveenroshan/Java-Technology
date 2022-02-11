3aac22f22d7191f3258f8a820fe946a63946d686
NaveenRoshan <thanks@NaveenRoshans-MacBook-Pro.local>
2022-02-11 13:01:29 +0530

    reactive program with spring

--- Reactive-Spring/reactive-spring-webflux-movieapp/movies-service/src/main/java/com/reactivespring/client/MovieInfoRestClient.java
+++ Reactive-Spring/reactive-spring-webflux-movieapp/movies-service/src/main/java/com/reactivespring/client/MovieInfoRestClient.java
@@ -1,16 +1,22 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class MovieInfoRestClient {
     private WebClient webClient;
 
-    //@Value("${restClient.MoviesInfoUrl}")
-    private String moviesInfoUrl = "http://localhost:8080/v1/movieinfos";
+    @Value("${restClient.MoviesInfoUrl}")
+    private String moviesInfoUrl;
 
     public MovieInfoRestClient(WebClient webClient) {
         this.webClient = webClient;
@@ -22,6 +28,23 @@ public class MovieInfoRestClient {
                 .get()
                 .uri(url,movieId)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    log.info("Status code is :" + clientResponse.statusCode().value());
+                    if(clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)){
+                        return Mono.error(
+                                new MoviesInfoClientException("There is no movie for the given Id" + movieId, clientResponse.statusCode().value()));
+                    }else {
+                        return clientResponse.bodyToMono(String.class)
+                                .flatMap(responseMessage ->
+                                        Mono.error(new MoviesInfoClientException(responseMessage, clientResponse.statusCode().value())));
+                    }
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                    log.info("Status code is :" + clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class)
+                                .flatMap(responseMessage ->
+                                        Mono.error(new MoviesInfoServerException("Server Exception in moviesInfoService :" + responseMessage)));
+                })
                 .bodyToMono(MovieInfo.class)
                 .log();
     }
--- Reactive-Spring/reactive-spring-webflux-movieapp/movies-service/src/main/java/com/reactivespring/client/MovieReviewRestClient.java
+++ Reactive-Spring/reactive-spring-webflux-movieapp/movies-service/src/main/java/com/reactivespring/client/MovieReviewRestClient.java
@@ -1,12 +1,19 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.Review;
+import com.reactivespring.exception.ReviewsClientException;
+import com.reactivespring.exception.ReviewsServerException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Component
+@Slf4j
 public class MovieReviewRestClient {
 
     private WebClient webClient;
@@ -15,12 +22,12 @@ public class MovieReviewRestClient {
         this.webClient =webClient;
     }
 
-   // @Value("${restClient.MoviesReviewUrl}")
-    private String moviesReviewUrl = "http://localhost:8081/v1/reviews";
+    @Value("${restClient.MoviesReviewUrl}")
+    private String moviesReviewUrl;
 
-    public Flux<Review> retrieveReviews(String moviesId){
+    public Flux<Review> retrieveReviews(String movieId){
         var url = UriComponentsBuilder.fromHttpUrl(moviesReviewUrl)
-                .queryParam("movieInfoId", moviesId)
+                .queryParam("movieInfoId", movieId)
                 .buildAndExpand()
                 .toUriString();
         System.out.println("RestClient Review URL: " + url);
@@ -28,6 +35,22 @@ public class MovieReviewRestClient {
                 .get()
                 .uri(url)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    log.info("Status code is :" + clientResponse.statusCode().value());
+                    if(clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)){
+                        return Mono.empty();
+                    }else {
+                        return clientResponse.bodyToMono(String.class)
+                                .flatMap(responseMessage ->
+                                        Mono.error(new ReviewsClientException(responseMessage)));
+                    }
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                    log.info("Status code is :" + clientResponse.statusCode().value());
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage ->
+                                    Mono.error(new ReviewsServerException("Server Exception in moviesReviewService :" + responseMessage)));
+                })
                 .bodyToFlux(Review.class);
     }
 }
